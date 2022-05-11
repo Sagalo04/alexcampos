7ddd70b7c8e03b6a77409d1fcb9f6bee3e652338
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-05-10 20:34:32 -0500

    btnControl

--- src/MindARViewer.jsx
+++ src/MindARViewer.jsx
@@ -1,26 +1,73 @@
-import { Loading } from "@nextui-org/react";
-import React, { useEffect, useRef } from "react";
+import { Link, Loading } from "@nextui-org/react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import boton from "./botonRA.png";
+
 function MindARViewer(props) {
-  //   const sceneRef = useRef(null);
-  //
-  //   useEffect(() => {
-  //     const sceneEl = sceneRef.current;
-  //     const arSystem = sceneEl.systems["mindar-image-system"];
-  //     sceneEl.addEventListener("renderstart", () => {
-  //       arSystem.start(); // start AR
-  //     });
-  //     return () => {
-  //       arSystem.stop();
-  //     };
-  //   }, []);
+  const [isBtn, SetisBtn] = useState(false);
+
+  const sceneRef = useRef(null);
+  const sceneRef2 = useRef();
+  const sceneRef3 = useRef();
+  const sceneRef4 = useRef();
+  const sceneRef5 = useRef();
+
+  useEffect(() => {
+    const sceneEl = sceneRef.current;
+    const sceneEl2 = sceneRef2.current;
+    const sceneEl3 = sceneRef3.current;
+    const sceneEl4 = sceneRef4.current;
+    const sceneEl5 = sceneRef5.current;
+
+    sceneEl.addEventListener("targetFound", () => {
+      SetisBtn(true);
+    });
+    sceneEl2.addEventListener("targetFound", () => {
+      SetisBtn(true);
+    });
+    sceneEl3.addEventListener("targetFound", () => {
+      SetisBtn(true);
+    });
+    sceneEl4.addEventListener("targetFound", () => {
+      SetisBtn(true);
+    });
+    sceneEl5.addEventListener("targetFound", () => {
+      SetisBtn(true);
+    });
+
+    sceneEl.addEventListener("targetLost", () => {
+      SetisBtn(false);
+    });
+    sceneEl2.addEventListener("targetLost", () => {
+      SetisBtn(false);
+    });
+    sceneEl3.addEventListener("targetLost", () => {
+      SetisBtn(false);
+    });
+    sceneEl4.addEventListener("targetLost", () => {
+      SetisBtn(false);
+    });
+    sceneEl5.addEventListener("targetLost", () => {
+      SetisBtn(false);
+    });
+  }, []);
+
   const URL2 = process.env.PUBLIC_URL + "finalscene.glb";
 
   return (
     <>
       <div id="example-scanning-overlay" className="hidden">
-        <Loading type="points" />Cargando
+        <Loading type="points" />
+        Cargando
       </div>
+      {isBtn ? (
+        <div className="buttonContainer">
+          <Link href="https://facebook.com">
+            <img src={boton} alt="" style={{ height: "85px" }} />
+          </Link>
+        </div>
+      ) : null}
+
       <a-scene
         mindar-image="uiLoading: #example-scanning-overlay;uiScanning: no; imageTargetSrc: ./targets.mind"
         color-space="sRGB"
@@ -34,7 +81,7 @@ function MindARViewer(props) {
 
         <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
 
-        <a-entity mindar-image-target="targetIndex: 0">
+        <a-entity ref={sceneRef} mindar-image-target="targetIndex: 0">
           <a-gltf-model
             rotation="0 0 0 "
             position="0 -0.25 0"
@@ -42,7 +89,7 @@ function MindARViewer(props) {
             src="#bearModel"
           />
         </a-entity>
-        <a-entity mindar-image-target="targetIndex: 1">
+        <a-entity ref={sceneRef2} mindar-image-target="targetIndex: 1">
           <a-gltf-model
             rotation="0 0 0 "
             position="0 -0.25 0"
@@ -50,7 +97,7 @@ function MindARViewer(props) {
             src="#bearModel"
           />
         </a-entity>
-        <a-entity mindar-image-target="targetIndex: 2">
+        <a-entity ref={sceneRef3} mindar-image-target="targetIndex: 2">
           <a-gltf-model
             rotation="0 0 0 "
             position="0 -0.25 0"
@@ -58,7 +105,7 @@ function MindARViewer(props) {
             src="#bearModel"
           />
         </a-entity>
-        <a-entity mindar-image-target="targetIndex: 3">
+        <a-entity ref={sceneRef4} mindar-image-target="targetIndex: 3">
           <a-gltf-model
             rotation="0 0 0 "
             position="0 -0.25 0"
@@ -66,7 +113,7 @@ function MindARViewer(props) {
             src="#bearModel"
           />
         </a-entity>
-        <a-entity mindar-image-target="targetIndex: 4">
+        <a-entity ref={sceneRef5} mindar-image-target="targetIndex: 4">
           <a-gltf-model
             rotation="0 0 0 "
             position="0 -0.25 0"
