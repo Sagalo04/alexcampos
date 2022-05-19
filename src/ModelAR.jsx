5525349ac4d53faf126d2085fe93f7629ec6cf3d
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-05-19 18:09:11 -0500

    New scale and link to the video

--- src/ModelAR.jsx
+++ src/ModelAR.jsx
@@ -6,7 +6,7 @@ function ModelAR({ targetIndex }) {
   useEffect(() => {
     const sceneEl1 = sceneRef1.current;
     sceneEl1.addEventListener("click", () => {
-      window.open("https://facebook.com");
+      window.open("https://youtu.be/1dwHcIATYKc");
     });
   }, []);
 
@@ -15,17 +15,17 @@ function ModelAR({ targetIndex }) {
       <a-gltf-model
         rotation="0 0 0 "
         position="0 -0.25 0"
-        scale="0.025 0.025 0.025"
+        scale="0.05 0.05 0.05"
         src="#bearModel"
       />
-      <a-text value="Jesús te trae" font="Roboto-Medium-msdf.json" scale="0.32 0.32 0.32" position="-0.2 -0.45 0.1" negate="false"></a-text>
-      <a-text value="AGUA VIVA" font="Roboto-Medium-msdf.json" scale="0.32 0.32 0.32" position="-0.18 -0.55 0.1" negate="false"></a-text>
+      <a-text value="Jesús te trae" font="Roboto-Medium-msdf.json" scale="0.6 0.6 0.6" position="-0.4 -0.75 0.1" negate="false"></a-text>
+      <a-text value="AGUA VIVA" font="Roboto-Medium-msdf.json" scale="0.6 0.6 0.6" position="-0.35 -0.9 0.1" negate="false"></a-text>
       <a-gltf-model
         class="clickable"
         ref={sceneRef1}
         rotation="0 0 0 "
-        position="0 -0.9 0.1"
-        scale="0.035 0.035 0.035"
+        position="0 -1.5 0.1"
+        scale="0.06 0.06 0.06"
         src="#boton"
       />
     </a-entity>
