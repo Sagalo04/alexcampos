408f372a0cab480033c4bdc94fa7a71f75ee5d0a
Sagalo04 <santiago.garcna@uao.edu.co>
2022-05-19 18:24:24 -0500

    numbers

--- src/MindARViewer.jsx
+++ src/MindARViewer.jsx
@@ -8,7 +8,7 @@ function MindARViewer(props) {
 
   useState(() => {
     let array = [];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 11; i++) {
       const element = "targetIndex: " + i;
       array.push(element);
     }
